1481136e8f1dbeced862b44c52d6da27bfa0f952
Dana1127 <danag20027@gmail.com>
2022-10-04 16:54:24 -0500

    Hacer correcciones generales de Primera entrega

---
+++ index.js
@@ -0,0 +1,11 @@
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 4,
+    centeredSlides: true,
+    spaceBetween: 30,
+    loop: true,
+    grabCursor: true,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      }
+  });
